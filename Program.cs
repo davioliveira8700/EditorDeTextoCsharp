2483d045a273fc9f385a115295f7233d3ce837d7
Davi de Oliveira <odavi278@gmail.com>
2024-01-01 13:50:14 -0300

    Iniciando projeto editor de texto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!!");
+        }
+
+    }
+
+}
